59543305f280aba10b20614a564aaba1cf865286
Uriel6u <122196184+Uriel6u@users.noreply.github.com>
2024-04-09 12:31:58 -0500

    Feat: image folder setup; userAuth visual improvements (#4)

--- userAuth/login.php
+++ userAuth/login.php
@@ -42,15 +42,19 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="userAuth.css"> 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    
     <title>MyLibrary: Login</title>
+
+    
 </head>
 
 <body>
     <div class="container">
-        <div style="background-color: darkgray;">
-            <form style="width: 50%; margin: auto;" method="post">
-                <h3 style="margin: 10px;">Login</h3>
+        <div class="login-form">
+            <form method="post">
+                <h3 style="text-align: center;">MyLibrary Login</h3>
 
                 <div class="form-group">
                     <label for="loginUsername">Username</label>
@@ -64,9 +68,8 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
                 <button type="submit" class="btn btn-success" name="Login">Login</button>
 
-                <div class="position-absolute top-50 start-50 translate-middle">
-                    <p>Don't have an account?</p>
-                    <a class="btn btn-primary" href="signup.php" role="button" style="margin: auto;">Signup</a>
+                <div class="signup-section">
+                    <p>Don't have an account? <a href="signup.php">Signup</a></p>
                 </div>
             </form>
         </div>
